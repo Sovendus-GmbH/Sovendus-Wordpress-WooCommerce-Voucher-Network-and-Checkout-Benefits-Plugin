8b057e9b10c23afd49611ed2ee5617876e040101
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-02-03 17:31:13 +0100

    fix build pipeline

--- wc-sovendus/phpstan-bootstrap.php
+++ wc-sovendus/phpstan-bootstrap.php
@@ -1,8 +1,16 @@
 <?php
-$wordpressPath = __DIR__ . '/vendor/johnpbloch/wordpress-core/wp-load.php';
+
+$wordpressPath = __DIR__ . '/vendor/johnpbloch/wordpress/wp-load.php';
+echo "Debug: Checking path $wordpressPath\n";
 if (file_exists($wordpressPath)) {
     require_once $wordpressPath;
 } else {
-    echo "Error: wp-load.php not found at $wordpressPath";
+    echo "Error: wp-load.php not found at $wordpressPath\n";
+    echo "Current directory: " . __DIR__ . "\n";
+    echo "Contents of vendor directory:\n";
+    $vendorContents = scandir(__DIR__ . '/vendor');
+    foreach ($vendorContents as $item) {
+        echo $item . "\n";
+    }
     exit(1);
 }

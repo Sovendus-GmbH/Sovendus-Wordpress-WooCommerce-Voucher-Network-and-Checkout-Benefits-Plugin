9d66208a95b359d731ad45360b608b6beb03f2ea
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-02-03 18:48:43 +0100

    fix build pipeline

--- wc-sovendus/phpstan-bootstrap.php
+++ wc-sovendus/phpstan-bootstrap.php
@@ -1,4 +1,1 @@
 <?php
-
-require_once __DIR__ . '/vendor/szepeviktor/phpstan-wordpress/stubs/wordpress-stubs.php';
-require_once __DIR__ . '/vendor/szepeviktor/phpstan-wordpress/stubs/woocommerce-stubs.php';

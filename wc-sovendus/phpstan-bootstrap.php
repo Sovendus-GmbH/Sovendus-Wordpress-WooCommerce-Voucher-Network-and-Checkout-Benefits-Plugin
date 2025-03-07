f29aba1834204756160fdd3f996e0d88dc002bc6
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-07 18:18:24 +0100

    fix linting issues

--- wc-sovendus/phpstan-bootstrap.php
+++ wc-sovendus/phpstan-bootstrap.php
@@ -1,1 +1,5 @@
 <?php
+
+require_once __DIR__ . '/vendor/szepeviktor/phpstan-wordpress/stubs/wordpress-stubs.php';
+
+require_once __DIR__ . '/vendor/szepeviktor/phpstan-wordpress/stubs/woocommerce-stubs.php';

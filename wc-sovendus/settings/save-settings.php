b94e69257751403a6014e8eb7de12e6ef82275dc
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-07 18:18:24 +0100

    fix linting issues

--- wc-sovendus/settings/save-settings.php
+++ wc-sovendus/settings/save-settings.php
@@ -15,7 +15,13 @@ function save_sovendus_settings()
     }
 
     $settings = isset($_POST['settings']) ? json_decode(sanitize_text_field(wp_unslash($_POST['settings'])), true) : array();
+    if (empty($settings)) {
+        wp_send_json_error('No settings provided');
+        exit;
+    }
+
     $validated_settings = Sovendus_App_Settings::fromJson($settings);
     update_option('sovendus_settings', wp_json_encode($validated_settings));
     wp_send_json_success($settings);
+    exit;
 }

a4d52d0833f29b278c7031ada801d896e783d74b
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-07 18:18:24 +0100

    fix linting issues

--- wc-sovendus/settings/save-settings.php
+++ wc-sovendus/settings/save-settings.php
@@ -4,24 +4,26 @@ defined('ABSPATH') || exit('WordPress Error! Opening plugin file directly');
 
 function save_sovendus_settings()
 {
+    // Inform PHPStan that check_ajax_referer can return both true and false
+    assert(function_exists('check_ajax_referer'));
+
     if (!check_ajax_referer('save_sovendus_settings_nonce', 'security', false)) {
         wp_send_json_error('Nonce check failed');
-        exit;
+        return;
     }
 
     if (!current_user_can('manage_options')) {
         wp_send_json_error('Unauthorized user');
-        exit;
+        return;
     }
 
     $settings = isset($_POST['settings']) ? json_decode(sanitize_text_field(wp_unslash($_POST['settings'])), true) : array();
     if (empty($settings)) {
         wp_send_json_error('No settings provided');
-        exit;
+        return;
     }
 
     $validated_settings = Sovendus_App_Settings::fromJson($settings);
     update_option('sovendus_settings', wp_json_encode($validated_settings));
     wp_send_json_success($settings);
-    exit;
 }
